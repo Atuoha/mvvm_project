225d71a99d1e43a03b638330b7ec57ab81e5b17c
Atuoha <atuohainitiatives@gmail.com>
2022-10-19 13:33:26 +0100

    fontManager

---
+++ lib/presentation/font_manager.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class FontConstants {
+  static const String fontFamily = "Montserrat";
+}
+
+class FontWeightManager {
+  static const FontWeight light = FontWeight.w300;
+  static const FontWeight normal = FontWeight.w400;
+  static const FontWeight bold = FontWeight.w700;
+  static const FontWeight semiBold = FontWeight.w600;
+  static const FontWeight medium = FontWeight.w500;
+}
