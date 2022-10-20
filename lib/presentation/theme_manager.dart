52e14d2579750a905654c2e1ca2f7c78b2b5b20e
Atuoha <atuohainitiatives@gmail.com>
2022-10-20 04:32:33 +0100

    textTheme

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mvvm_project/presentation/styles_manager.dart';
 import 'package:mvvm_project/presentation/values_manager.dart';
-
 import 'color_manager.dart';
 import 'font_manager.dart';
 
@@ -12,13 +11,37 @@ ThemeData getAppTheme() {
     primaryColorDark: ColorManager.darkPrimary,
     disabledColor: ColorManager.grey1,
     splashColor: ColorManager.primaryOpacity,
+
+    // card theme
     cardTheme: CardTheme(
       color: ColorManager.white,
       shadowColor: ColorManager.grey,
       elevation: AppSize.s4,
     ),
-    buttonTheme: ButtonTheme(child: child),
+
+    // button theme
+    buttonTheme: ButtonThemeData(
+      buttonColor: ColorManager.primaryColor,
+      shape: const StadiumBorder(),
+      disabledColor: ColorManager.grey1,
+      splashColor: ColorManager.primaryOpacity,
+    ),
+
+    // elevated button theme
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(AppSize.s12),
+        ),
+        backgroundColor: ColorManager.primaryColor,
+        textStyle: getRegularStyle(color: ColorManager.white),
+      ),
+    ),
+
+    // input decoration theme
     inputDecorationTheme: InputDecorationTheme(),
+
+    // app bar theme
     appBarTheme: AppBarTheme(
       color: ColorManager.primaryColor,
       elevation: AppSize.s4,
@@ -26,10 +49,29 @@ ThemeData getAppTheme() {
       centerTitle: true,
       titleTextStyle: getRegularStyle(
         color: ColorManager.darkPrimary,
-        fontSize: FontSize.s18,
+        fontSize: FontSize.s16,
       ),
     ),
-    textTheme: TextTheme(),
+
+    // text theme
+    textTheme: TextTheme(
+      headline1: getSemiBoldStyle(
+        color: ColorManager.grey,
+        fontSize: FontSize.s16,
+      ),
+      subtitle1: getMediumStyle(
+        color: ColorManager.liteGrey,
+        fontSize: FontSize.s14,
+      ),
+      caption: getRegularStyle(
+        color: ColorManager.grey1,
+        fontSize: FontSize.s12,
+      ),
+      bodyText1: getRegularStyle(
+        color: ColorManager.grey,
+      ),
+    ),
+
     colorScheme: ColorScheme.fromSwatch().copyWith(
       secondary: ColorManager.grey,
     ),
