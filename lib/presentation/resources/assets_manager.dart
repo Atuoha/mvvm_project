e3bc91e3700ce77d8956784ef73edf7592cf913d
Atuoha <atuohainitiatives@gmail.com>
2022-10-24 06:37:24 +0100

    onBoarding -assets setup

--- lib/presentation/resources/assets_manager.dart
+++ lib/presentation/resources/assets_manager.dart
@@ -4,4 +4,8 @@ const String assetPath = "assets/images";
 class ImageAsset{
   static const splashImage = '$assetPath/splash_logo.png';
   static const unknownRouteImage = '$assetPath/404.png';
+  static const onBoardingImage1 = '$assetPath/sp1.png';
+  static const onBoardingImage2 = '$assetPath/sp2.png';
+  static const onBoardingImage3 = '$assetPath/sp3.png';
+  static const onBoardingImage4 = '$assetPath/sp4.png';
 }
--- lib/presentation/resources/string_manager.dart
+++ lib/presentation/resources/string_manager.dart
@@ -1,5 +1,13 @@
 class AppString{
   static const unknownPage = "404! Page not found";
-  static const onBoardingTitle1 = "See the best ";
+  static const onBoardingTitle1 = "Welcome to Learnie";
+  static const onBoardingTitle2 = "A home for all learners";
+  static const onBoardingTitle3 = "Building a community of learners";
+  static const onBoardingTitle4 = "Let's learn!";
+
+  static const onBoardingSubTitle1 = "Learnie is an app to get the best of learning";
+  static const onBoardingSubTitle2 = "the acquisition of knowledge or skills through study";
+  static const onBoardingSubTitle3 = "the ability to learn is possessed by humans";
+  static const onBoardingSubTitle4 = "a process that leads to change, which incurs growth";
 
 }
