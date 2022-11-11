20752e140779579a8a6eca4623b32b9ffeae4f75
Atuoha <atuohainitiatives@gmail.com>
2022-11-11 08:31:16 +0100

    Task: configuring for json

---
+++ lib/data/responses.dart
@@ -0,0 +1,9 @@
+import 'package:json_annotation/json_annotation.dart';
+
+@JsonSerializable()
+class BaseResponse {
+  @JsonKey(name: "status")
+  int? status;
+  @JsonKey(name: "message")
+  String? message;
+}
