c9635707afe73f25afc260fbb940df8429d1c214
Atuoha <atuohainitiatives@gmail.com>
2022-11-14 12:11:13 +0100

    Task: working with network app section

---
+++ lib/app/constants.dart
@@ -0,0 +1,3 @@
+class Constants{
+  static const String baseUrl = "https://learnie.mocklab.io/";
+}
---
+++ lib/data/network/app_api.dart
@@ -0,0 +1,18 @@
+import 'package:retrofit/http.dart';
+import '../../app/constants.dart';
+import 'package:dio/dio.dart';
+
+import '../responses/responses.dart';
+
+part 'app_api.g.dart';
+
+@RestApi(baseUrl: Constants.baseUrl)
+abstract class AppServiceClient {
+  factory AppServiceClient(Dio dio, {String baseUrl}) = _AppServiceClient;
+
+  @POST("/customers/login")
+  Future<AuthenticateResponse> login();
+}
+
+
+
