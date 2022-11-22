ce849f0731c6ce11d7a1ef2bff93ca31998ed0ed
Atuoha <atuohainitiatives@gmail.com>
2022-11-22 16:28:54 +0100

    Task: login entries  implementations

--- lib/presentation/authentication/login/login.dart
+++ lib/presentation/authentication/login/login.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:mvvm_project/presentation/authentication/login/login_viewmodel.dart';
+import 'package:mvvm_project/presentation/resources/values_manager.dart';
+
+import '../../resources/assets_manager.dart';
+import '../../resources/color_manager.dart';
+import '../../resources/font_manager.dart';
+import '../../resources/string_manager.dart';
+import '../../resources/styles_manager.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -10,6 +17,7 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
   final LoginViewModel _loginViewModel = LoginViewModel(null);
+  final _formKey = GlobalKey<FormState>();
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
@@ -29,6 +37,12 @@ class _LoginScreenState extends State<LoginScreen> {
     bind();
   }
 
+  login() {}
+
+  navigateToForgotPass() {}
+
+  navigateToSignUp() {}
+
   @override
   void dispose() {
     super.dispose();
@@ -37,6 +51,91 @@ class _LoginScreenState extends State<LoginScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.only(
+          top: AppPadding.p100,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset(ImageAsset.splashImage),
+            Text(
+              AppString.appName,
+              style: getBoldStyle(
+                color: ColorManager.primaryColor,
+                fontSize: FontSize.s18,
+              ),
+            ),
+            const SizedBox(height: AppSize.s12),
+            Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  Padding(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: AppPadding.p28),
+                    child: StreamBuilder<bool>(
+                      stream: _loginViewModel.outputIsUserNameValid,
+                      builder: (context, snapshot) => TextFormField(
+                        controller: _usernameController,
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                            hintText: 'Enter Username',
+                            labelText: 'Username',
+                            errorText: (snapshot.data ?? true)
+                                ? null
+                                : 'Username has error'),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Padding(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: AppPadding.p28),
+                    child: StreamBuilder<bool>(
+                      stream: _loginViewModel.outputIsPasswordValid,
+                      builder: (context, snapshot) => TextFormField(
+                        keyboardType: TextInputType.visiblePassword,
+                        controller: _passwordController,
+                        decoration: InputDecoration(
+                            hintText: 'Enter Password',
+                            labelText: 'Password',
+                            errorText: (snapshot.data ?? true)
+                                ? null
+                                : 'Password has error'),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: AppSize.s28),
+                  ElevatedButton(
+                    onPressed: () => login,
+                    child: const Text('Login'),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      TextButton(
+                        onPressed: () => navigateToForgotPass(),
+                        child: const Text(
+                          'Forgot Password',
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () => navigateToSignUp(),
+                        child: const Text(
+                          'Not a member? Signup',
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
