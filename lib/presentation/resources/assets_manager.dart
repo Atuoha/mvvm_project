6801bc71d3b9e97ee8f0bac9e5b0c6123d5cf811
Atuoha <atuohainitiatives@gmail.com>
2022-11-29 13:26:51 +0100

    Task: state renderer impl

--- lib/presentation/resources/assets_manager.dart
+++ lib/presentation/resources/assets_manager.dart
@@ -1,11 +1,19 @@
-const String assetPath = "assets/images";
+const String imagePath = "assets/images";
+const String jsonPath = "assets/json";
 
 
 class ImageAsset{
-  static const splashImage = '$assetPath/splash.png';
-  static const unknownRouteImage = '$assetPath/404.png';
-  static const onBoardingImage1 = '$assetPath/sp1.png';
-  static const onBoardingImage2 = '$assetPath/sp2.png';
-  static const onBoardingImage3 = '$assetPath/sp3.png';
-  static const onBoardingImage4 = '$assetPath/sp4.png';
+  static const splashImage = '$imagePath/splash.png';
+  static const unknownRouteImage = '$imagePath/404.png';
+  static const onBoardingImage1 = '$imagePath/sp1.png';
+  static const onBoardingImage2 = '$imagePath/sp2.png';
+  static const onBoardingImage3 = '$imagePath/sp3.png';
+  static const onBoardingImage4 = '$imagePath/sp4.png';
+}
+
+class JsonAsset{
+  static const emptyImg = '$jsonPath/empty.json';
+  static const errorImg = '$jsonPath/error.json';
+  static const loadingImg = '$jsonPath/loading.json';
+  static const successImg = '$jsonPath/success.json';
 }
